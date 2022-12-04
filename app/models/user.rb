c0b070931ed562ada2703dd2bbe435c3b6d25225
parovstelar <113955451+parovstelar@users.noreply.github.com>
2022-12-04 19:06:26 +0000

    [finish] 課題３

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,13 @@ class User < ApplicationRecord
 
   # belongs_to :books
   has_many :books, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
 
-
+  has_many :favorites, dependent: :destroy
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
